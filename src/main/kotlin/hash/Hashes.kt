4a613e55daf424f3c8a40d092766fcef5191d4a9
Kim Fritzon-Ölander <kim.fritzon@gmail.com>
2021-03-27 17:29:27 +0100

    Change from kt-file to class

--- src/main/kotlin/hash/Hashes.kt
+++ src/main/kotlin/hash/Hashes.kt
@@ -3,31 +3,36 @@ package hash
 import java.security.MessageDigest
 import javax.xml.bind.DatatypeConverter
 
-fun main() {
-    hashSimplified()
-}
+class HashCretor {
 
-fun hashSimplified() {
-    val message = "Hello world"
-    val doPrint = true
-    hashValue(message, MD5, doPrint)
-    hashValue(message, SHA256, doPrint)
-    hashValue(message, SHA512, doPrint)
-}
+    fun hashSimplified() {
+        val message = "Hello world"
+        val doPrint = true
+        getHashValue(message, MD5, doPrint)
+        getHashValue(message, SHA256, doPrint)
+        getHashValue(message, SHA512, doPrint)
+    }
 
-fun hashValue(message: String, algorithm: String, printHash: Boolean = false): String {
-    val value = digestString(message, algorithm)
-    if (printHash) { println("$value ($algorithm)") }
-    return value
-}
+    private fun getHashValue(message: String,
+                             algorithm: String,
+                             printHash: Boolean = false
+    ): String {
+        val value = digestString(message, algorithm)
+        if (printHash) { println("$value ($algorithm)") }
+        return value
+    }
 
-private fun digestString(message: String, algorithm: String): String {
-    val messageDigest = MessageDigest.getInstance(algorithm)
-    messageDigest.update(message.toByteArray())
-    val digest = messageDigest.digest()
-    return DatatypeConverter.printHexBinary(digest).toUpperCase()
-}
+    private fun digestString(message: String, algorithm: String): String {
+        val messageDigest = MessageDigest.getInstance(algorithm)
+        messageDigest.update(message.toByteArray())
+        val digest = messageDigest.digest()
+        return DatatypeConverter.printHexBinary(digest).toUpperCase()
+    }
 
-private const val MD5 = "MD5"
-private const val SHA256 = "SHA-256"
-private const val SHA512 = "SHA-512"
+    companion object {
+
+        private const val MD5 = "MD5"
+        private const val SHA256 = "SHA-256"
+        private const val SHA512 = "SHA-512"
+    }
+}
